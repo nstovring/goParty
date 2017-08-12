41995fe193304d02873af1cc94f6b8768fa35830
Nikolaj Støvring <nstovring@live.dk>
2017-08-12 20:37:53 +0200

    Setting up client

---
+++ goParty/goParty/Abstractions/ICloudService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace goParty.Abstractions
+{
+    interface ICloudService
+    {
+        ICloudTable<T> GetTable<T>() where T : TableData;
+    }
+}
---
+++ goParty/goParty/Services/AzureCloudService.cs
@@ -0,0 +1,23 @@
+﻿using goParty.Abstractions;
+using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace goParty.Services
+{
+    public class AzureCloudService : ICloudService
+    {
+        MobileServiceClient client;
+
+        public AzureCloudService()
+        {
+            client = new MobileServiceClient("https://gopartyc3799bc758f544faa52787e94a24730d.azurewebsites.net");
+        }
+
+        public ICloudTable<T> GetTable<T>() where T : TableData
+        {
+            return new AzureCloudTable<T>(client);
+        }
+    }
+}
